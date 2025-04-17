ccc46f9e3625321996e3cf5dc12f80e97f13f991
Chiodoo <marco.chiodo03@gmail.com>
2025-04-17 12:55:20 +0200

    Finita entity Campagna

--- src/main/java/it/uniroma3/siw/model/Personaggio.java
+++ src/main/java/it/uniroma3/siw/model/Personaggio.java
@@ -1,9 +1,14 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Personaggio {
@@ -11,4 +16,26 @@ public class Personaggio {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String razza;
+
+    @Column(nullable = false)
+    private String classe;
+
+    @Column(nullable = false)
+    private Long livello;
+
+    @Column(length = 2000)
+    private String descrizione;
+
+    @OneToOne
+    private Caratteristiche caratteristiche;
+
+    @ManyToMany
+    private List<Campagna> campagne;
+
 }
