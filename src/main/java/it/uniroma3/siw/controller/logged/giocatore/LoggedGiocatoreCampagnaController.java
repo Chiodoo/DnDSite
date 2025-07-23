5a96df4b15647f192e2b86a6266a08317abaab11
Lars276491 <laura.straccini@gmail.com>
2025-07-23 22:41:23 +0200

    aggiunte note in giocatore campagna

--- src/main/java/it/uniroma3/siw/controller/logged/giocatore/LoggedGiocatoreCampagnaController.java
+++ src/main/java/it/uniroma3/siw/controller/logged/giocatore/LoggedGiocatoreCampagnaController.java
@@ -7,14 +7,17 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.uniroma3.siw.model.Campagna;
+import it.uniroma3.siw.model.Nota;
 import it.uniroma3.siw.model.Personaggio;
 import it.uniroma3.siw.security.SecurityUtils;
 import it.uniroma3.siw.service.CampagnaService;
@@ -133,4 +136,12 @@ public class LoggedGiocatoreCampagnaController {
         model.addAttribute("campagnePartecipate", campagne);
         return "logged/giocatore/giocCampagnePartecipate";
     }
+    
+    
+
+    
+    
+   
+
+
 }
