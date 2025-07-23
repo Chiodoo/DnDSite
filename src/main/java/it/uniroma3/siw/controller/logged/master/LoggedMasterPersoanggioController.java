4f83bf754c52fb4e9e998f36263dee456c4e6486
Lars276491 <laura.straccini@gmail.com>
2025-07-23 17:27:52 +0200

    sistemato personaggio template master

--- src/main/java/it/uniroma3/siw/controller/logged/master/LoggedMasterPersoanggioController.java
+++ src/main/java/it/uniroma3/siw/controller/logged/master/LoggedMasterPersoanggioController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.uniroma3.siw.service.*;
 import it.uniroma3.siw.model.*;
@@ -38,5 +39,20 @@ public class LoggedMasterPersoanggioController {
         return "logged/master/masterPersonaggio";
     }
 
+    @GetMapping("/campagna/{id}/personaggio/{personaggioId}")
+    public String getPersonaggioDaCampagna(@PathVariable Long id,
+                                             @PathVariable Long personaggioId,
+                                              Model model,
+                                              RedirectAttributes redirectAttrs) {
+        Optional<Personaggio> optionalPersonaggio = personaggioService.findById(personaggioId);
+        if (!optionalPersonaggio.isPresent()) {
+            model.addAttribute("errorMessage", "Personaggio non trovato.");
+            return "redirect:/logged/master/campagna/" + id; // o pagina 404
+        }
+        Personaggio personaggio = optionalPersonaggio.get();
+        model.addAttribute("personaggio", personaggio);
+        model.addAttribute("campagnaId", id);
+        return "logged/master/masterPersonaggio";
+    }
     
 }
